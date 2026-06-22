7d70877965391d083b896877ee7a8ec97e55eec4
Ilse Ramirez <ilsetacho476@gmail.com>
2026-06-22 15:12:10 -0600

    feat: google tag manager

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -42,14 +42,29 @@ export default defineNuxtConfig({
             fbq('track', 'PageView');
         `,
         type: 'text/javascript'
+    },
+    {
+        innerHTML: `
+                    (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                    })(window,document,'script','dataLayer','GTM-KK969THR');
+                `,
+                type: 'text/javascript'
     }
 ],
 noscript: [
     {
         innerHTML: `<img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=1287239663026510&ev=PageView&noscript=1"/>`
+    },
+    {
+        innerHTML: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KK969THR" height="0" width="0" style="display:none;visibility:hidden"></iframe>`
     }
 ]
     }
 }
 
 })
+
+
